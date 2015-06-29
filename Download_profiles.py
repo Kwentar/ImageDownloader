b7df96a127776f1dfccabcd1f904e08e4c74cc13
Aleksey <alekseev.yeskela@gmail.com>
2015-06-29 19:58:39 +0300

    full info about user added

--- Download_profiles.py
+++ Download_profiles.py
@@ -3,9 +3,11 @@ from Internet import Internet
 from vk import Vk
 
 
-vk_dir = 'D:\\Graphics\\vk\\'
-need_reload_file = 'D:\\Graphics\\vk\\need_reload.txt'
-downloaded_users_file = 'D:\\Graphics\\vk\\downloaded_users.txt'
+vk_dir = 'F:\\vk\\'
+need_reload_file = 'F:\\vk\\need_reload.txt'
+downloaded_users_file = 'F:\\vk\\downloaded_users.txt'
+users_info_file = 'F:\\vk\\downloaded_users_info.txt'
+
 
 def check_in_file(uid, lst):
     founded = False
@@ -16,8 +18,9 @@ def check_in_file(uid, lst):
     return founded
 
 
-def get_profile_photos(id_, start_dir):
-    dir_of_photos = os.path.join(start_dir, id_)
+def get_profile_photos(id_, start_dir, age):
+    dir_of_photos = os.path.join(start_dir, age.__str__())
+    dir_of_photos = os.path.join(dir_of_photos, id_)
     if not os.path.exists(dir_of_photos):
         os.makedirs(dir_of_photos)
     images = Vk.get_profile_photos(id_)
@@ -34,17 +37,27 @@ def download_users(age_from, age_to, city_id):
               7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
     with open(downloaded_users_file, 'r') as downloaded_users:
         downloaded_users_list = downloaded_users.readlines()
+    with open(users_info_file, 'r',  encoding='utf-8') as users_info:
+        users_info_lines = users_info.readlines()
     for age in range(age_from, age_to+1):
         for month in months.keys():
             for day in range(1, months[month]+1):
                 print('''processed: day = {}, month = {}, age = {}'''.format(day, month, age))
-                uids = Vk.get_uids(age, month, day, city_id)
-                for uid in uids:
-                    if check_in_file(uid, downloaded_users_list):
-                        print('We have this user!')
+                users = Vk.get_uids(age, month, day, city_id)
+                need_to_write_users = list()
+                for user in users:
+                    if not check_in_file(user.uid, downloaded_users_list):
+                        get_profile_photos(user.uid, vk_dir, age)
+                    if check_in_file(user.uid, users_info_lines):
+                        print('We have this user in info too! ' + user.name + ' ' + user.last_name)
                     else:
-                        get_profile_photos(uid, vk_dir)
+                        print('added info ' + user.name + ' ' + user.last_name)
+                        need_to_write_users.append(user)
+                with open(users_info_file, 'a+',  encoding='utf-8') as users_info:
+                    for user in need_to_write_users:
+                        users_info.write(user.__str__() + '\n')
 
-download_users(19, 24, 10)
+
+download_users(19, 20, 10)
 
 
--- vk.py
+++ vk.py
@@ -1,7 +1,9 @@
 import json
+from urllib.error import URLError
 from urllib.parse import urlencode
 from urllib.request import urlopen
 import time
+from datetime import date
 from Profiler import Profiler
 import __setup_photo__ as setup
 
@@ -12,6 +14,24 @@ class VkError(Exception):
 
     def __str__(self):
         return repr(self.value)
+
+
+class VkUser:
+    def __init__(self, uid, name, last_name, day_b, month_b, sex, age):
+        self.uid = uid
+        self.name = name
+        self.last_name = last_name
+        self.day_b = day_b
+        self.month_b = month_b
+        year_b = date.today().year - age
+        if month_b < date.today().month or month_b == date.today().month and day_b < date.today().day:
+            year_b - 1
+        self.year_b = year_b
+        self.sex = sex
+
+    def __str__(self):
+        return ";".join([self.uid, self.name, self.last_name, self.day_b.__str__(),
+                         self.month_b.__str__(), self.year_b.__str__(), self.sex.__str__()])
 
 
 class Vk:
@@ -44,6 +64,7 @@ class Vk:
 
     @staticmethod
     def call_api(method, params):
+        Vk.check_time()
         if not Vk.curr_token:
             Vk.curr_token = Vk.get_token()
         if isinstance(params, list):
@@ -55,30 +76,30 @@ class Vk:
 
         params_list += [('access_token', Vk.curr_token)]
         url = 'https://api.vk.com/method/%s?%s' % (method, urlencode(params_list))
-
-        response = urlopen(url).read()
-        result = json.loads(response.decode('utf-8'))
         try:
-            if 'response' in result.keys():
-                return result['response']
-            else:
-                raise VkError('no response on answer: ' + result['error']['error_msg'].__str__())
-        except VkError as err:
-            print(err.value)
-            Vk.curr_token = Vk.get_token()
-            Vk.call_api(method, params)
-            return list()
+            response = urlopen(url).read()
+            result = json.loads(response.decode('utf-8'))
+            try:
+                if 'response' in result.keys():
+                    return result['response']
+                else:
+                    raise VkError('no response on answer: ' + result['error']['error_msg'].__str__())
+            except VkError as err:
+                print(err.value)
+                Vk.curr_token = Vk.get_token()
+                Vk.call_api(method, params)
+        except URLError as err:
+            print('URLError: ' + err.errno.__str__() + ", " + err.reason.__str__())
+
+        return list()
 
     @staticmethod
-    def get_user_info(id_, fields='sex'):
-        pass
-
-    @staticmethod
-    def get_uids(age, month, day, city_id):
+    def get_uids(age, month, day, city_id, fields='sex'):
         search_q = list()
         search_q.append(('offset', '0'))
         search_q.append(('count', '1000'))
         search_q.append(('city', city_id))
+        search_q.append(('fields', fields))
         search_q.append(('age_from', age))
         search_q.append(('age_to', age))
         search_q.append(('has_photo', '1'))
@@ -86,17 +107,19 @@ class Vk:
         search_q.append(('birth_month', month))
         r = Vk.call_api('users.search', search_q)
         count = 0
-        uids = list()
+        users = list()
         for el in r:
             if count and 'uid' in el.keys():
-                uid = el['uid'].__str__()
-                uids.append(uid)
+                user = VkUser(uid=el['uid'].__str__(), name=el['first_name'].__str__(),
+                              last_name=el['last_name'].__str__(), sex=el['sex'].__str__(),
+                              day_b=day, month_b=month, age=age)
+                users.append(user)
             else:
                 count = el
         if count > 1000:
             Vk.warning('''Count more than 1000, count = {}, age = {},
                         month = {}, day = {}'''.format(count, age, month, day))
-        return uids
+        return users
 
     @staticmethod
     def get_profile_photos(id_):
