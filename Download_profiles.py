8bf8feddbae0d6616ff79e99b8a5c3c959cd3cfc
Aleksey <alekseev.yeskela@gmail.com>
2015-07-02 19:01:04 +0300

    added friends download features

--- Download_profiles.py
+++ Download_profiles.py
@@ -5,7 +5,7 @@ from vk import Vk
 
 vk_dir = 'F:\\vk\\'
 need_reload_file = 'F:\\vk\\need_reload.txt'
-downloaded_users_file = 'F:\\vk\\downloaded_users.txt'
+#downloaded_users_file = 'F:\\vk\\downloaded_users.txt'
 users_info_file = 'F:\\vk\\downloaded_users_info.txt'
 
 
@@ -18,9 +18,8 @@ def check_in_file(uid, lst):
     return founded
 
 
-def get_profile_photos(id_, start_dir, age):
-    dir_of_photos = os.path.join(start_dir, age.__str__())
-    dir_of_photos = os.path.join(dir_of_photos, id_)
+def get_profile_photos(id_, start_dir, downloaded_users_file):
+    dir_of_photos = os.path.join(start_dir, id_)
     if not os.path.exists(dir_of_photos):
         os.makedirs(dir_of_photos)
     images = Vk.get_profile_photos(id_)
@@ -32,32 +31,88 @@ def get_profile_photos(id_, start_dir, age):
             downloaded_users.write(id_ + '\n')
 
 
-def download_users(age_from, age_to, city_id):
+def write_users_to_file(need_to_write_users, users_info_file_friends):
+    with open(users_info_file_friends, 'a+', encoding='utf-8') as users_info:
+        for user in need_to_write_users:
+            users_info.write(user.__str__() + '\n')
+
+
+def get_lists_from_file(file_name):
+    with open(file_name, 'a+') as downloaded_users:
+        downloaded_users.seek(0)
+        return downloaded_users.readlines()
+
+
+def write_users_in_file(file_name, users, open_mode='a+'):
+    with open(file_name, open_mode,  encoding='utf-8') as users_info_fr:
+        for user in users:
+            users_info_fr.write(user.__str__() + '\n')
+
+
+def download_users(age_from, age_to, city_id, downloaded_users_file):
     months = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30,
               7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
-    with open(downloaded_users_file, 'r') as downloaded_users:
-        downloaded_users_list = downloaded_users.readlines()
-    with open(users_info_file, 'r',  encoding='utf-8') as users_info:
-        users_info_lines = users_info.readlines()
+    downloaded_users_list = get_lists_from_file(downloaded_users_file)
+    users_info_list = get_lists_from_file(users_info_file)
     for age in range(age_from, age_to+1):
         for month in months.keys():
             for day in range(1, months[month]+1):
                 print('''processed: day = {}, month = {}, age = {}'''.format(day, month, age))
-                users = Vk.get_uids(age, month, day, city_id)
+                while True:
+                    users = Vk.get_uids(age, month, day, city_id)
+                    if users:
+                        break
+                    Vk.get_token()
                 need_to_write_users = list()
                 for user in users:
                     if not check_in_file(user.uid, downloaded_users_list):
-                        get_profile_photos(user.uid, vk_dir, age)
-                    if check_in_file(user.uid, users_info_lines):
+                        dir_of_photos = os.path.join(vk_dir, age.__str__())
+                        get_profile_photos(user.uid, dir_of_photos, downloaded_users_file)
+                    if check_in_file(user.uid, users_info_list):
                         print('We have this user in info too! ' + user.name + ' ' + user.last_name)
                     else:
                         print('added info ' + user.name + ' ' + user.last_name)
                         need_to_write_users.append(user)
-                with open(users_info_file, 'a+',  encoding='utf-8') as users_info:
-                    for user in need_to_write_users:
-                        users_info.write(user.__str__() + '\n')
+                write_users_to_file(need_to_write_users, users_info_file)
 
 
-download_users(19, 20, 10)
+def downloaded_friends(start_id, dir_, deep=1):
+    downloaded_users_file_friends = os.path.join(dir_, 'downloaded_users.txt')
+    users_info_file_friends = os.path.join(dir_, 'user_info.txt')
+    downloaded_users_list = get_lists_from_file(downloaded_users_file_friends)
+    users_info_list = get_lists_from_file(users_info_file_friends)
+    while True:
+        users = Vk.get_friends(start_id)
+        if users:
+            break
+        Vk.get_token()
+    if not check_in_file(start_id, downloaded_users_list):
+        get_profile_photos(start_id, dir_, downloaded_users_file_friends)
+    need_to_write_users = list()
+    write_users_in_file(os.path.join(dir_, start_id + '\\friends.txt'), users, open_mode='w')
+    count = 0
+    for user in users:
+        count += 1
+        if not check_in_file(user.uid, downloaded_users_list):
+            get_profile_photos(user.uid, dir_,  downloaded_users_file_friends)
+        if check_in_file(user.uid, users_info_list):
+            print('We have this user in info too! ' + user.name + ' ' + user.last_name)
+        else:
+            print('added info ' + user.name + ' ' + user.last_name + ' (friend of ' + start_id.__str__() + '), '
+                  + count.__str__() + '\\' + len(users).__str__())
+            need_to_write_users.append(user)
+        while True:
+            user_fr = Vk.get_friends(user.uid)
+            if user_fr:
+                break
+            Vk.get_token()
+        write_users_in_file(os.path.join(dir_, user.uid + '\\friends.txt'), user_fr, open_mode='w')
+        if deep:
+            downloaded_friends(user.uid, dir_, deep-1)
+    write_users_to_file(need_to_write_users, users_info_file_friends)
+
+
+downloaded_friends('11152217', 'F:\\vk\\friends\\')
+# download_users(23, 23, 10, 'F:\\vk\\downloaded_users.txt')
 
 
--- vk.py
+++ vk.py
@@ -17,7 +17,7 @@ class VkError(Exception):
 
 
 class VkUser:
-    def __init__(self, uid, name, last_name, day_b, month_b, sex, age):
+    def __init__(self, uid, name, last_name, day_b, month_b, sex, age, city_id):
         self.uid = uid
         self.name = name
         self.last_name = last_name
@@ -28,10 +28,14 @@ class VkUser:
             year_b - 1
         self.year_b = year_b
         self.sex = sex
+        self.city_id = city_id
 
     def __str__(self):
         return ";".join([self.uid, self.name, self.last_name, self.day_b.__str__(),
-                         self.month_b.__str__(), self.year_b.__str__(), self.sex.__str__()])
+                         self.month_b.__str__(), self.year_b.__str__(), self.sex.__str__(), self.city_id.__str__()])
+
+    def get_age(self):
+        return date.today().year - self.year_b
 
 
 class Vk:
@@ -40,9 +44,9 @@ class Vk:
     p = Profiler()
 
     @staticmethod
-    def check_time():
-        if Vk.p.get_time() < 0.5:
-            time.sleep(0.5)
+    def check_time(value=0.5):
+        if Vk.p.get_time() < value:
+            time.sleep(value)
         Vk.p.start()
 
     @staticmethod
@@ -53,20 +57,22 @@ class Vk:
     @staticmethod
     def get_token():
         for el in Vk.tokens:
-            test_url = 'https://api.vk.com/method/getProfiles?uid=66748&access_token=' + el
-            Vk.check_time()
-            response = urlopen(test_url).read()
-            result = json.loads(response.decode('utf-8'))
-            if 'response' in result.keys():
-                print('now I use the ' + el + ' token')
-                return el
+            if el != Vk.curr_token:
+                test_url = 'https://api.vk.com/method/getProfiles?uid=66748&access_token=' + el
+                Vk.check_time(10)
+                response = urlopen(test_url).read()
+                result = json.loads(response.decode('utf-8'))
+                if 'response' in result.keys():
+                    print('now I use the ' + el + ' token')
+                    Vk.curr_token = el
+                    return el
         raise VkError('all tokens are invalid: ' + result['error']['error_msg'].__str__())
 
     @staticmethod
     def call_api(method, params):
         Vk.check_time()
-        if not Vk.curr_token:
-            Vk.curr_token = Vk.get_token()
+        while not Vk.curr_token:
+            Vk.get_token()
         if isinstance(params, list):
             params_list = params[:]
         elif isinstance(params, dict):
@@ -112,13 +118,56 @@ class Vk:
             if count and 'uid' in el.keys():
                 user = VkUser(uid=el['uid'].__str__(), name=el['first_name'].__str__(),
                               last_name=el['last_name'].__str__(), sex=el['sex'].__str__(),
-                              day_b=day, month_b=month, age=age)
+                              day_b=day, month_b=month, age=age, city_id=city_id)
                 users.append(user)
             else:
                 count = el
         if count > 1000:
             Vk.warning('''Count more than 1000, count = {}, age = {},
                         month = {}, day = {}'''.format(count, age, month, day))
+        return users
+
+    @staticmethod
+    def get_friends(uid, fields='city,bdate,sex'):
+        search_q = list()
+        search_q.append(('user_id', uid))
+        search_q.append(('offset', '0'))
+        search_q.append(('count', '1000'))
+        search_q.append(('fields', fields))
+        r = Vk.call_api('friends.get', search_q)
+        count = len(r)
+        users = list()
+        for el in r:
+            if 'user_id' in el.keys():
+                if 'deactivated' in el.keys():
+                    continue
+                uid = el['user_id'].__str__()
+                last_name = 'None'
+                sex = 'None'
+                name = 'None'
+                city_id = 'None'
+                day, month, age = [0, 0, 0]
+                if 'last_name' in el.keys():
+                    last_name = el['last_name'].__str__()
+                if 'first_name' in el.keys():
+                    name = el['first_name'].__str__()
+                if 'sex' in el.keys():
+                    sex = el['sex'].__str__()
+                if 'city' in el.keys():
+                    city_id = el['city'].__str__()
+                if 'bdate' in el.keys():
+                    bdate = el['bdate'].__str__().split('.')
+                    if len(bdate) > 2:
+                        day, month, age = map(int, bdate)
+                        age = date.today().year - age
+                    else:
+                        day, month = map(int, bdate)
+
+                user = VkUser(uid=uid, name=name, last_name=last_name, sex=sex, day_b=day,
+                              month_b=month, age=age, city_id=city_id)
+                users.append(user)
+        if count > 1000:
+            Vk.warning('Count more than 1000')
         return users
 
     @staticmethod
