8e7db0eb60b6a6e2a3e41a06714c171cc979f992
Kwentar <alekseev.yeskela@gmail.com>
2015-07-26 23:40:47 +0300

    added function for downloading images by tag from instagram

--- InstagramDownloader.py
+++ InstagramDownloader.py
@@ -15,12 +15,40 @@ def get_user_id_by_name(instagram_api, user_name):
         return None
 
 
-def download_user_photos(instagram_api, user_name, dir_):
+def download_photos_by_tag(instagram_api, tag, dir_):
+    """
+    Download all photos with this tag
+    :param instagram_api: instagram api by python-instagram package
+    :param tag: tag for search
+    :param dir_: dir for photos
+    :return: None
+    """
+    (response, next_) = instagram_api.tag_recent_media(tag_name=tag, count=100)
+    image_list = list()
+    max_id_str = 'max_tag_id='
+    while True:
+        image_list += [el.images['standard_resolution'].url for el in response]
+        print('current count is {}'.format(len(image_list)))
+        if next_ is not None and max_id_str in next_:
+            max_index = next_.find(max_id_str) + len(max_id_str)
+            max_id = next_[max_index:]
+        else:
+            break
+        (response, next_) = instagram_api.tag_recent_media(tag_name=tag, count=100, max_tag_id=max_id)
+    print("count of images for downloading: {}".format(len(image_list)))
+    dir_ = os.path.join(dir_, tag)
+    if not os.path.exists(dir_):
+        os.mkdir(dir_)
+    Internet.load_images(image_list, dir_, failed_image_urls_file="need_reload.txt", delay=300)
+
+
+def download_user_photos(instagram_api, user_name, dir_, tag=''):
     """
     download all photos of instagram user to dir_/user_name folder
     :param instagram_api: instagram api by python-instagram package
     :param user_name: instagram user name
     :param dir_: dir for photos
+    :param tag: download user photos with this tag, if empty - all photos
     :return: None
     """
     user_id = get_user_id_by_name(instagram_api, user_name)
@@ -31,7 +59,13 @@ def download_user_photos(instagram_api, user_name, dir_):
     max_id_str = 'max_id='
     (response, next_) = instagram_api.user_recent_media(user_id=user_id, count=100)
     while True:
-        image_list += [el.images['standard_resolution'].url for el in response]
+        for el in response:
+            if tag:
+                for t in el.tags:
+                    if tag == t.name:
+                        image_list.append(el.images['standard_resolution'].url)
+            else:
+                image_list.append(el.images['standard_resolution'].url)
         print('current count is {}'.format(len(image_list)))
         if next_ is not None and max_id_str in next_:
             max_index = next_.find(max_id_str) + len(max_id_str)
@@ -45,6 +79,9 @@ def download_user_photos(instagram_api, user_name, dir_):
         os.mkdir(dir_)
     Internet.load_images(image_list, dir_, failed_image_urls_file="need_reload.txt", delay=300)
 
+
 access_token = setup.inst_access_token
 api = InstagramAPI(access_token=setup.inst_access_token)
-download_user_photos(api, 'kwentar4ik', '/home/instagram')
+# download_user_photos(api, '', 'D:\\Graphics\\Download\\instagram', tag='killfish')
+download_photos_by_tag(api, 'emptycity', 'D:\\Graphics\\Download\\instagram')
+# (response, next_) = instagram_api.tag_recent_media(tag_name='tag', count=100, max_tag_id=max_id)
